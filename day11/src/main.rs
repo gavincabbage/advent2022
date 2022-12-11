9b6adb5f51b4179bc879b0e8be4c24d87bdd5e15
Gavin Cabbage <dev@gavincabbage.com>
2022-12-11 18:37:59 -0500

    cycles maybe

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,5 @@
 use rust_embed::RustEmbed;
+use primes::factors_uniq;
 
 #[derive(RustEmbed)]
 #[folder = "data/"]
@@ -13,10 +14,10 @@ fn main() {
     let input_file = Data::get("input.txt").unwrap();
     let input_data = std::str::from_utf8(input_file.data.as_ref()).unwrap();
     assert_eq!(151312, part1(input_data));
-    // assert_eq!(0, part2(input_data));
+    assert_eq!(0, part2(input_data));
 }
 
-fn part1(data: &str) -> u128 {
+fn part1(data: &str) -> u64 {
     let mut monkeys: Vec<Monkey> = parse(data);
     for _ in 0..20 {
         for mndx in 0..monkeys.len() {
@@ -24,7 +25,7 @@ fn part1(data: &str) -> u128 {
                 let mut x = monkeys[mndx].items[indx];
                 x = (monkeys[mndx].op)(x);
                 x = x / 3;
-                let dest: u128 = (monkeys[mndx].test)(x);
+                let dest: u64 = (monkeys[mndx].test)(x);
                 monkeys[dest as usize].items.push(x);
                 monkeys[mndx].inspections += 1;
             }
@@ -38,14 +39,15 @@ fn part1(data: &str) -> u128 {
     })
 }
 
-fn part2(data: &str) -> u128 {
-    let mut monkeys: Vec<Monkey> = parse(data);
+fn part2(data: &str) -> u64 {
+    let mut monkeys: Vec<Monkey> = parse2(data);
     for _ in 0..10_000 {
         for mndx in 0..monkeys.len() {
             for indx in 0..monkeys[mndx].items.len() {
                 let mut x = monkeys[mndx].items[indx];
                 x = (monkeys[mndx].op)(x);
-                let dest: u128 = (monkeys[mndx].test)(x);
+                let dest: u64 = (monkeys[mndx].test)(x);
+                // x = reduce(x);
                 monkeys[dest as usize].items.push(x);
                 monkeys[mndx].inspections += 1;
             }
@@ -60,10 +62,10 @@ fn part2(data: &str) -> u128 {
 }
 
 struct Monkey {
-    items: Vec<u128>,
-    op: Box<dyn Fn(u128) -> u128>,
-    test: Box<dyn Fn(u128) -> u128>,
-    inspections: u128,
+    items: Vec<u64>,
+    op: Box<dyn Fn(u64) -> u64>,
+    test: Box<dyn Fn(u64) -> u64>,
+    inspections: u64,
 }
 
 fn parse(data: &str) -> Vec<Monkey> {
@@ -80,40 +82,75 @@ fn parse(data: &str) -> Vec<Monkey> {
     monkeys
 }
 
-fn parse_items(data: &str) -> Vec<u128> {
-    let mut items: Vec<u128> = Vec::new();
+
+fn parse2(data: &str) -> Vec<Monkey> {
+    let mut monkeys: Vec<Monkey> = Vec::new();
+    data.split("\n\n").for_each(|monkey| {
+        let lines: Vec<&str> = monkey.lines().collect();
+        monkeys.push(Monkey{
+            items: parse_items(lines[1]),
+            op: parse_op2(lines[2]),
+            test: parse_test(&lines[3..=5]),
+            inspections: 0,
+        });
+    });
+    monkeys
+}
+
+fn parse_items(data: &str) -> Vec<u64> {
+    let mut items: Vec<u64> = Vec::new();
     data.strip_prefix("  Starting items: ").unwrap().split(",").for_each(|s| {
         items.push(s.trim().parse().unwrap());
     });
     items
 }
 
-fn parse_op(data: &str) ->  Box<dyn Fn(u128) -> u128> {
+fn parse_op(data: &str) ->  Box<dyn Fn(u64) -> u64> {
     let split: Vec<&str> = data.split_whitespace().rev().take(2).collect();
-    match split[0].trim().parse::<u128>() {
+    match split[0].trim().parse::<u64>() {
         Ok(i) => {
             if split[1] == "+" {
-                Box::new(move |n: u128| -> u128 { n + i })
+                Box::new(move |n: u64| -> u64 { n + i })
             } else {
-                Box::new(move |n: u128| -> u128 { n * i })
+                Box::new(move |n: u64| -> u64 { n * i })
             }
         },
-        _ => Box::new(move |n: u128| -> u128 { n * n })
+        _ => Box::new(move |n: u64| -> u64 { n * n })
     }
 }
 
-fn parse_test(data: &[&str]) ->  Box<dyn Fn(u128) -> u128> {
+fn parse_op2(data: &str) ->  Box<dyn Fn(u64) -> u64> {
+    let split: Vec<&str> = data.split_whitespace().rev().take(2).collect();
+    match split[0].trim().parse::<u64>() {
+        Ok(i) => {
+            if split[1] == "+" {
+                Box::new(move |n: u64| -> u64 { n + i })
+            } else {
+                Box::new(move |n: u64| -> u64 { n + (n % i) })
+            }
+        },
+        _ => Box::new(move |n: u64| -> u64 { n })
+    }
+}
+
+fn parse_test(data: &[&str]) ->  Box<dyn Fn(u64) -> u64> {
     let get_last = |s: &str| {
         s.split_whitespace().last().unwrap().parse().unwrap()
     };
-    let divisor: u128 = get_last(data[0]);
-    let td: u128 = get_last(data[1]);
-    let fd: u128 = get_last(data[2]);
-    Box::new(move |n: u128| -> u128 {
+    let divisor: u64 = get_last(data[0]);
+    let td: u64 = get_last(data[1]);
+    let fd: u64 = get_last(data[2]);
+    Box::new(move |n: u64| -> u64 {
         if n % divisor == 0 {
             td
         } else {
             fd
         }
     })
+}
+
+fn reduce(x: u64) -> u64 {
+    let r = factors_uniq(x).iter().fold(1, |i, f| if f < &1000 { i * f } else { i });
+    println!("{} {} {:?}", x, r, factors_uniq(x));
+    r
 }
